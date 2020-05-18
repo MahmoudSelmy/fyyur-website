1ee59038d4350329855f9649dc4d6a5315126fbf
mahmoudselmy06@gmail.com <mahmoud_sam_06>
2020-05-18 04:13:48 +0200

    + show venue

--- models/venue.py
+++ models/venue.py
@@ -24,8 +24,8 @@ class Venue(db.Model):
 
     def convert_to_dict(self):
         upcoming_count = (db.session.query(func.count(Show.c.venue_id))
-                            .filter(Show.c.venue_id == self.id)
-                            .filter(Show.c.start_time > datetime.now()).all()[0][0])
+            .filter(Show.c.venue_id == self.id)
+            .filter(Show.c.start_time > datetime.now()).all()[0][0])
         return {
             'id': self.id,
             'name': self.name,
@@ -40,3 +40,45 @@ class Venue(db.Model):
             "data": venues
         }
         return response
+
+    @classmethod
+    def get_all_venues_grouped_by_area(cls):
+        data = db.session.query(Venue.city, cls.state).group_by(cls.city, cls.state)
+        venues_data = []
+        for a in data:
+            area = a._asdict()
+            area['venues'] = [ven.convert_to_dict() for ven in cls.query.filter_by(city=area['city']).all()]
+            venues_data.append(area)
+        return venues_data
+
+    @classmethod
+    def _get_show_data(cls, Artist, venue_id, date_filter_function):
+        shows = (db.session.query(Artist.id.label("artist_id"), Artist.name.label("artist_name"),
+                                  Artist.image_link.label("artist_image_link"), Show)
+                 .filter(Show.c.venue_id == venue_id)
+                 .filter(date_filter_function(Show.c.start_time, datetime.now()))
+                 .all())
+        return shows
+
+    @classmethod
+    def get_upcoming_shows(cls, Artist, venue_id):
+        def date_filter_function(show_time, current_time):
+            return show_time > current_time
+
+        return cls._get_show_data(Artist, venue_id, date_filter_function)
+
+    @classmethod
+    def get_past_shows(cls, Artist, venue_id):
+        def date_filter_function(show_time, current_time):
+            return show_time <= current_time
+
+        return cls._get_show_data(Artist, venue_id, date_filter_function)
+
+    @classmethod
+    def get_venue_show_data(cls, Artist, venue_id):
+        venue = cls.query.get(venue_id)
+        venue.past_shows = cls.get_past_shows(Artist, venue_id)
+        venue.upcoming_shows = cls.get_upcoming_shows(Artist, venue_id)
+        venue.past_shows_count = len(venue.past_shows)
+        venue.upcoming_shows_count = len(venue.upcoming_shows)
+        return venue
