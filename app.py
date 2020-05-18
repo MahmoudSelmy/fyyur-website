daa22504df3b042fd3f54be20b60e4e5f417e927
mahmoudselmy06@gmail.com <mahmoud_sam_06>
2020-05-18 02:17:14 +0200

    + create artist

--- app.py
+++ app.py
@@ -400,16 +400,30 @@ def create_artist_form():
     return render_template('forms/new_artist.html', form=form)
 
 
+def submit_artist():
+    new_artist = Artist(
+        name=request.form['name'],
+        city=request.form['city'],
+        state=request.form['state'],
+        phone=request.form['phone'],
+        genres=request.form.getlist('genres'),
+        facebook_link=request.form['facebook_link']
+    )
+    db.session.add(new_artist)
+    db.session.commit()
+
+
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-    # called upon submitting the new artist listing form
-    # TODO: insert form data as a new Venue record in the db, instead
-    # TODO: modify data to be the data object returned from db insertion
-
-    # on successful db insert, flash success
-    flash('Artist ' + request.form['name'] + ' was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+    try:
+        submit_artist()
+        # on successful db insert, flash success
+        flash('Venue ' + request.form['name'] + ' was successfully listed!')
+    except Exception as e:
+        print(e)
+        flash('An error occurred. Artist ' + request.form['name'] + ' could not be listed.')
+    finally:
+        db.session.close()
     return render_template('pages/home.html')
 
 
